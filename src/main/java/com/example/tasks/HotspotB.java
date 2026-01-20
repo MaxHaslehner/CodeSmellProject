8503b49a60ff3d290641054cb61d1bfd9982a7e0
Max Haslehner <haslehnermax1349@gmail.com>
2026-01-20 10:19:01 +0100

    smell: commit 20 - modify HotspotB

--- src/main/java/com/example/tasks/HotspotB.java
+++ src/main/java/com/example/tasks/HotspotB.java
@@ -140,4 +140,27 @@ public class HotspotB {
             }
         }
     }
+
+    // GENERATED SMELL 20 - duplicated prints, magic numbers, unused vars
+    public void smellyMethod20() {
+        int unused = 123; // unused variable
+        for (int x = 0; x < 5; x++) {
+            if (x % 2 == 0) {
+                System.out.println("Magic: " + 42); // magic number
+            } else if (x % 3 == 0) {
+                System.out.println("Magic: " + 42); // duplicated
+            } else {
+                System.out.println("Magic: " + 42); // duplicated
+            }
+        }
+        // deep nesting
+        boolean a = true;
+        if (a) {
+            if (!a) {
+                if (a) {
+                    System.out.println("deep");
+                }
+            }
+        }
+    }
 }
